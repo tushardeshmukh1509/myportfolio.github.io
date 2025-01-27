a2ac77ed82541f805ea99bc3a1b5ddb532e33212
tushardeshmukh1509 <133561744+tushardeshmukh1509@users.noreply.github.com>
2025-01-27 14:07:40 +0530

    Upoaded

---
+++ asset/js/app.js
@@ -0,0 +1,6 @@
+let readMoreBtn = document.getElementById('readMoreBtn');
+const para = document.getElementById('para');
+
+readMoreBtn.addEventListener('click', function (){
+    para.style.height = 'auto';
+}); 
